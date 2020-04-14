131047b0904a10ff6b3b13d037c52db2e8765a11
Tom Piere <tpiere@uship.com>
2020-04-14 15:34:50 -0700

    refactored tests to use react testing library

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,43 @@
-import { shallow } from "enzyme";
-import React from "react";
-
+import * as nearbyPlacesService from '../services/nearbyPlaces';
 import App from "../pages/index.js";
 
-describe("With Enzyme", () => {
-  it('App shows a main heading', () => {
-    const app = shallow(<App />);
-    expect(app.find("h1").text()).toEqual("Find nearby places of interest");
+// import dependencies
+import React from 'react'
+
+// import react-testing methods
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect'
+
+
+jest.mock('../services/nearbyPlaces')
+
+describe("App", () => {
+  beforeEach(() => {
+    //   jest.resetAll();
+  })
+  it('renders a main heading', () => {
+    const { container, asFragment } = render(<App />)
+    expect(screen.getByRole('heading')).toHaveTextContent('Find nearby places of interest')
   });
+
+  it('displays nearby places', async () => {
+    nearbyPlacesService.getNearbyPlaces = jest.fn().mockResolvedValue(
+      [
+        { id: 1, name: 'place 1', formattedAddressParts: ['111 1st street', 'Seattle', 'WA'] }
+      ]
+    );
+
+    
+      const { findByText, getByLabelText } = render(<App />)
+
+      fireEvent.change(getByLabelText('Enter a location'), { target: { value: 'Seattle, WA' } })
+    
+      const searchButton = await findByText('Search');
+      fireEvent.click(searchButton);
+      
+    const firstResult = await findByText('place 1');
+    expect(firstResult).toBeDefined();
+  })
 });
