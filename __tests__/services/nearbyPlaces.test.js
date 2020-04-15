040154c14e6f0d5e4d007fadc0a95f0879db907a
Tom Piere <tpiere@uship.com>
2020-04-15 11:13:45 -0700

    added test case for an unsucessful request

--- __tests__/services/nearbyPlaces.test.js
+++ __tests__/services/nearbyPlaces.test.js
@@ -35,12 +35,35 @@ describe('nearbyPlaces service', () => {
         )
         const places = await getNearbyPlaces('Tacoma');
 
-        expect(places).toEqual([{ "formattedAddressParts": [
-            "1101 Tacoma Ave S (at S 11th St)", "Tacoma, WA 98402", "United States"], 
-            "id": "4baa8fd4f964a520cd743ae3", "name": "Tower Coffee Bar And Grill" }, 
-            { "formattedAddressParts": ["1102 Tacoma Ave S", "Tacoma, WA 98402", "United States"], 
-            "id": "4b8d5e96f964a5208ef732e3", 
-            "name": "Tacoma Public Library - Main" }])
+        expect(places).toEqual([{
+            "formattedAddressParts": [
+                "1101 Tacoma Ave S (at S 11th St)", "Tacoma, WA 98402", "United States"],
+            "id": "4baa8fd4f964a520cd743ae3", "name": "Tower Coffee Bar And Grill"
+        },
+        {
+            "formattedAddressParts": ["1102 Tacoma Ave S", "Tacoma, WA 98402", "United States"],
+            "id": "4b8d5e96f964a5208ef732e3",
+            "name": "Tacoma Public Library - Main"
+        }])
+    });
+
+    it('should throw an error when the request is not successful', async () => {
+
+        getVenues.mockResolvedValue(
+            {
+                "meta": { "code": 400, errorDetail: "Couldn't geocode param near: fremont brewery" },
+                "response": {}
+
+            }
+
+        )
+        expect.assertions(1);
+        try {
+          await getNearbyPlaces('fremont brewery');
+        } catch (e) {
+          expect(e.message).toMatch("Couldn't geocode param near: fremont brewery");
+        }
+
     });
 
 
