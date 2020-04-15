30a3ea2f8aa82296b0a7e30c68eb0641d5d48d64
Tom Piere <tpiere@uship.com>
2020-04-15 13:11:07 -0700

    more styling

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
     const [searchError, setSearchError] = useState(null);
 
     async function submitSearch(e) {
-        console.log(`search for: ${searchInput}`);
         e.preventDefault();
         try {
             const venues = await getNearbyPlaces(searchInput);
@@ -27,25 +26,69 @@ export default function Home() {
         return false;
     }
 
+    const headingStyles = {
+        textAlign:'center',
+        lineHeight: 1.25
+    }
+
+    const formStyles = {
+        textAlign:'center'
+    }
+
+    const baseFormFieldStyles = {
+        display:'block',
+        textAlign:'center',
+        marginTop: 20
+    }
+
+    const horizontalCenterStyles = {
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
+
+    const textLabelStyles = {
+        ...baseFormFieldStyles
+    };
+    const textInputStyles = {
+        ...baseFormFieldStyles,
+        ...horizontalCenterStyles,
+        width: '100%',
+        maxWidth: '500px',
+        border: '1px solid #ddd',
+        padding: 4
+        
+    };
+    const buttonStyles = {
+        ...baseFormFieldStyles,
+        ...horizontalCenterStyles,
+        boxSizing: 'border-box',
+        padding: 8,
+        border: '2px solid #aaa'
+    };
+
+    const errorStyles = {
+        textAlign: 'center',
+        color: '#d64040'
+    }
     return (
         <Layout>
             <div>
                 <Head>
                     <title>Carta Challenge</title>
                 </Head>
-                <h1 >Find nearby places of interest</h1>
-                <form onSubmit={submitSearch}>
-                    <label htmlFor="locationInput">Enter a location</label>
+                <h1 style={headingStyles}>Find nearby places of interest</h1>
+                <form style={formStyles} onSubmit={submitSearch}>
+                    <label style={textLabelStyles} htmlFor="locationInput">Enter a location</label>
                     <input type="text" 
                     placeholder="Seattle, WA" 
                     id="locationInput" 
                     value={searchInput} 
-                    onChange={(event) => { setSearchInput(event.target.value); }} />
-                    <button type="submit">Search</button>
-
+                    onChange={(event) => { setSearchInput(event.target.value); }} 
+                    style={textInputStyles}/>
+                    <button style={buttonStyles} type="submit">Search</button>
                 </form>
                 <VenueList nearbyPlaces={nearbyPlaces}/>
-                {searchError != null && <span>{searchError}</span>}
+                {searchError != null && <p style={errorStyles}>{searchError}</p>}
             </div>
         </Layout>
     );
