dfa1441806a6482bba1f7f403d8497393a8095a7
Tom Piere <tpiere@uship.com>
2020-04-15 16:25:06 -0700

    wip: add geolocating

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getNearbyPlaces } from "../services/nearbyPlaces";
 import { VenueList } from "../components/VenueList";
+import { MdMyLocation } from 'react-icons/md';
+import { findByLabelText } from "@testing-library/react";
 
 const headingStyles = {
   textAlign: "center",
@@ -28,12 +30,9 @@ const textLabelStyles = {
   ...baseFormFieldStyles,
 };
 const textInputStyles = {
-  ...baseFormFieldStyles,
-  ...horizontalCenterStyles,
-  width: "100%",
-  maxWidth: "500px",
   border: "1px solid #ddd",
   padding: 4,
+  flex: '1 1 100%'
 };
 const buttonStyles = {
   ...baseFormFieldStyles,
@@ -52,6 +51,13 @@ export default function Home() {
   const [searchInput, setSearchInput] = useState("");
   const [nearbyPlaces, setNearbyPlaces] = useState(null);
   const [searchError, setSearchError] = useState(null);
+  const [hasGeolocationAvailable, setHasGeolocationAvailable] = useState(false);
+
+  useEffect(() => {
+    if(typeof window !== 'undefined' && window.navigator?.geolocation != null){
+        setHasGeolocationAvailable(true);
+      }    
+  }, []);
 
   async function submitSearch(e) {
     e.preventDefault();
@@ -78,7 +84,17 @@ export default function Home() {
           <label style={textLabelStyles} htmlFor="locationInput">
             Enter a location
           </label>
-          <input
+          <div
+            style={{
+                width: '100%',
+                maxWidth: 600,
+                display: 'flex',
+                ...horizontalCenterStyles,
+                marginTop: 10
+            }
+            }
+          >
+            <input
             type="text"
             placeholder="Seattle, WA"
             id="locationInput"
@@ -87,7 +103,35 @@ export default function Home() {
               setSearchInput(event.target.value);
             }}
             style={textInputStyles}
-          />
+            />
+          {hasGeolocationAvailable &&
+           <button style={{flex: '0 0 auto', marginLeft: 10, padding:6}} type="button" onClick={() => {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const {latitude, longitude} = position.coords;
+                    // console.log(position.coords.latitude, position.coords.longitude);
+                    setSearchInput(`${latitude}, ${longitude}`);
+                });
+
+           }}>
+            <MdMyLocation style={{verticalAlign: 'text-top'}}/>
+            <span>Use current location</span>
+            <style jsx>
+                {`
+                    span {
+                        display: none;
+                    }
+                    @media screen and (min-width: 767px){
+                        span {
+                            display: inline;
+                            margin-left: 5px;
+                        }
+                    }
+                `}
+            </style>
+          </button>
+          }
+          </div>
+          
           <button style={buttonStyles} type="submit">
             Search
           </button>
